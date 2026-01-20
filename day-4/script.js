614ee802ca730842a44b41ce7b4f1e73edaaff70
Kumari Pratibha <kp939331@gmail.com>
2026-01-20 14:56:25 +0530

    day 4

---
+++ day-4/script.js
@@ -0,0 +1,13 @@
+let a1=[1,2,3];
+let a2=[6,8,9];
+console.log([...a1,...a2]) //spread operator
+
+let age=2;
+let canvote=age>=18?"yes":"no rajasthani can not vote, only bihari can";
+console.log(canvote);
+
+
+
+
+
+
