7443df8be54e886b6dce6015c07c1acb7bafd52b
Kumari Pratibha <kp939331@gmail.com>
2026-01-20 19:05:16 +0530

    day 4

--- day-4/script.js
+++ day-4/script.js
@@ -4,10 +4,16 @@ console.log([...a1,...a2]) //spread operator
 
 let age=2;
 let canvote=age>=18?"yes":"no rajasthani can not vote, only bihari can";
-console.log(canvote);
+//console.log(canvote);
 
+//even or not
 
+let no=7;
+let e=no%2==0?"even":"odd";
+//console.log(e)
 
-
-
+//check a year is leap year or not
+let year=2026;
+let y= (year%4==0 && year%100!=0) || (year%400==0) ?"yes":"no";
+console.log(y);
 
