774640aa2c08304d6335e95ba32954084ad68952
Kumari Pratibha <kp939331@gmail.com>
2026-01-27 15:20:21 +0530

    day6-2

--- day-6/script.js
+++ day-6/script.js
@@ -14,6 +14,25 @@ console.log(`my 2nd favp is ${user.favp[1]}---------${user.demo()}`)
 
 
 const keys=Object.keys(user)
-console.log(keys)
+//console.log(keys)
 const val=Object.values(user)
-console.log(val)
+//console.log(val)
+console.log(Object.entries(user))
+
+// [
+//     ["x","o","x"],
+//     ["o","x","o"],
+//     ["x","o","o"]
+// ]
+
+//find the winner 
+
+Object.seal(user) //we can not add or modify but in seal we can modify but not add
+user.name="kpppppppppppppppppp";
+
+console.log(user.name)
+//console.log(user.moi)
+
+
+
+
