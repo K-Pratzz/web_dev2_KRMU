08d2d8d59d0ee5fc87849c6a22f5304445461803
Kumari Pratibha <kp939331@gmail.com>
2026-02-02 15:36:06 +0530

    2

--- day-8/script.js
+++ day-8/script.js
@@ -1,3 +1,9 @@
 const head=document.getElementById("h");
 head.style.color="tomato";
 head.style.backgroundColor="yellow"
+
+const para=document.getElementsByClassName("para");
+para[0].style.color="red";
+const parr=document.getElementsByTagName("p");
+console.log(parr);
+parr[1].style.color="cyan";
