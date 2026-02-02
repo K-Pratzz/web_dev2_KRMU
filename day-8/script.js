9ec26c27a4aa4db4b84d9a4a073af37b18aaea29
Kumari Pratibha <kp939331@gmail.com>
2026-02-02 15:53:18 +0530

    3

--- day-8/script.js
+++ day-8/script.js
@@ -6,4 +6,8 @@ const para=document.getElementsByClassName("para");
 para[0].style.color="red";
 const parr=document.getElementsByTagName("p");
 console.log(parr);
-parr[1].style.color="cyan";
+parr[1].style.color="green";
+parr[1].textContent="HIIIIIIIIIII PRATZZ"
+
+const qs=document.querySelector("#c p")
+qs.textContent="1233333333333333";
