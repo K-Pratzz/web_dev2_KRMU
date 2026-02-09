b74ae6944a41cbbe265f974fde97542da93c6056
K-Pratzz <kp939331@gmail.com>
2026-02-09 19:42:43 +0530

    assignment-1 html css

---
+++ assignment-1/script.js
@@ -0,0 +1,15 @@
+//target the froms .\, button 
+let sampleEvent=[{
+    name :"hi",
+    date:25,
+    category:"x",
+    description:"y"
+},{
+    name:"bye",
+    date:25,
+    category:"y",
+    description:"zz"
+
+}]
+
+//make html css of assignment 1
