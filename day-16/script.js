02e18dcbf90208f3f54132ef13f18cc151f1c407
K-Pratzz <kp939331@gmail.com>
2026-02-23 20:35:01 +0530

    day 16

---
+++ day-16/script.js
@@ -0,0 +1,29 @@
+// console.log("frist line");
+// setTimeout(()=>{
+//     console.log("timeout")
+// },0)
+
+// const p=new Promise((res,rej)=>{
+//     res()
+// })
+// p.then(()=>{
+//     console.log("p")
+// })
+
+
+// const p1=new Promise((res,rej)=>{
+//     res()
+// })
+// p1.then(()=>{
+//     console.log("p1")
+// })
+// console.log("last");
+
+const options = {method: 'GET', headers: {Authorization: 'Bearer zpka_6c51b610bf044abdbad944bdc7a2ccad_b46eb199'}};
+
+fetch('https://dataservice.accuweather.com/currentconditions/v1/202345', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err));
+  
+//202396 delhi
