4a6335984c38f7b74dfef404a0c83a2205ea473c
K-Pratzz <kp939331@gmail.com>
2026-02-23 20:41:05 +0530

    nnn

--- day-16/script.js
+++ day-16/script.js
@@ -21,7 +21,7 @@
 
 const options = {method: 'GET', headers: {Authorization: 'Bearer YOUR_APIKEY'}};
 
-fetch('https://dataservice.accuweather.com/currentconditions/v1/202345', options)
+fetch('x', options)
   .then(response => response.json())
   .then(response => console.log(response))
   .catch(err => console.error(err));
