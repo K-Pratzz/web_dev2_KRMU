4fe52196a7d5b78b8bd21fad7b1e5201a2ddad69
K-Pratzz <kp939331@gmail.com>
2026-03-17 18:53:15 +0530

    day 20

--- day-19/script.js
+++ day-19/script.js
@@ -1,13 +1,14 @@
-async function getdata(city){
-    try{
-        const api="0201f292126f678a397019e6aabd74df";
-        const res= await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid={api}`);
-        const json= await res.json();
-        console.log(json)
+async function fetchData(city) {
+    try {
+        const API_key = "0201f292126f678a397019e6aabd74df";
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_key}&units=metric`);
 
-    }catch(err){
-        console.error(err)
+        const data = await response.json();
+        console.log(city);
+        console.log(data.main.temp);
+        console.log(data.main.humidity);
+    } catch (err) {
+        console.error(err);
     }
-    
 }
-getdata("gurugram");
+fetchData("london")
